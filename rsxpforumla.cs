8b11e669b472f5e790ceda68b9461f51032be057
Danny <Gingerbreadpk@gmail.com>
2019-03-07 13:51:43 -0600

    Reformat Code
    Removed unnecessary variable and fixed the grammar.

--- rsxpforumla.cs
+++ rsxpforumla.cs
@@ -7,24 +7,18 @@ namespace ConsoleApp2
 
         static void Main()
         {
-            float level = 1;
+            float level = 2;
             double lvl = 1;
             double points = 0;
             double output = 0;
-            double currentXP = 0;
             double xpToLevel = 83;
             double xpForLevel = 83;
             double difference = 0;
 
-            while (level < 99 && lvl < 99)
+            while (level < 100 && lvl < 100)
             {
-                if (level == 1)
+                if (level > 2)
                 {
-                    currentXP += xpToLevel;
-                }
-                else
-                {
-                    currentXP += xpToLevel;
                     xpToLevel += (xpToLevel * 0.1f);
                     int i = (int)Math.Round(xpToLevel);
                     xpForLevel = xpForLevel + i;
@@ -33,8 +27,8 @@ namespace ConsoleApp2
                 output = Math.Floor(points / 4);
                 difference = output - xpForLevel;
                 Console.WriteLine("Level: " + level.ToString());
-                Console.WriteLine("XP Needed for next Level at 10%: " + String.Format("{0:n0}", xpForLevel) + "xp.");
-                Console.WriteLine("Actual XP Needed for next Level in-game: " + String.Format("{0:n0}", output) + "xp.");
+                Console.WriteLine("XP Needed for this Level at 10%: " + String.Format("{0:n0}", xpForLevel) + "xp.");
+                Console.WriteLine("Actual XP Needed for this Level in-game: " + String.Format("{0:n0}", output) + "xp.");
                 Console.WriteLine("That is a difference of: " + String.Format("{0:n0}", difference) + "xp.");
                 Console.WriteLine("");
                 level++;
